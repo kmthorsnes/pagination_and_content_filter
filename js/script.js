4d81b267332945e7645707c07fa2ca780b1fb7e0
Karl-Magnus Thorsnes <karl-magnus.thorsnes@kantar.com>
2018-10-25 16:48:18 +0200

    work in progress

--- js/script.js
+++ js/script.js
@@ -13,22 +13,46 @@ const StudentsPerPage = 10;
 let totalPages = Math.ceil(numItems / StudentsPerPage);
 
 let showStudent = document.getElementsByClassName('student-item');
+let numberofstudent = document.getElementsByClassName('student-item').length;
 
-
-console.log(totalPages, numItems, StudentsPerPage, showStudent, showStudent.length);
+console.log(totalPages, numItems, StudentsPerPage, showStudent, showStudent.length, numberofstudent);
 
 // Create a function to hide all of the items in the list excpet for the ten you want to show
 // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
 
-const showPage = () =>
-{
-for (i = StudentsPerPage; i < showStudent.length; i ++) {
-    showStudent[i].style.display = 'none';
-    }
+const showPage = (list, page) => {
+    for (i = 0; i < showStudent.length; i++)
+        if (i < StudentsPerPage){
+            showStudent[i].style.display = 'block';
+        } else {
+            showStudent[i].style.display = 'none';
+        }
 };
 
 // Runs showPage function
 showPage();
+
+// Create and append the pagination links - Creating a function that can do this is a good approach
+const appendPageLinks = () => {
+    // determine how many pages are needed for the list by dividing the total number of list items by the max number of items per page
+    // create a div, give it the “pagination” class, and append it to the .page div
+    let newDiv = document.createElement('div');
+    newDiv.className = "pagination";
+    document.getElementById(".page")(page);
+    // add a ul to the “pagination” div
+    // for every page
+    // add li and a tags with the page number text
+    // add an event listener to each a tag, or add an event listener to the pagination div, and use event delegation to target the a tags to define what happens they are clicked
+    // calls the showPage function to display the appropriate page
+    // loop over pagination links to remove active class from all
+    // add the active class to the link that was just clicked, otherwise known as the
+    event.target
+};
+
+// Add functionality to the pagination buttons so that they show and hide the correct items
+// Tip: If you created a function above to show/hide list items, it could be helpful here
+
+
 
 
 // $("#hide").click(function(){
@@ -52,11 +76,7 @@ showPage();
 //$('.student-item').addClass("after");
 
 
-// Create and append the pagination links - Creating a function that can do this is a good approach
 
-
-// Add functionality to the pagination buttons so that they show and hide the correct items
-// Tip: If you created a function above to show/hide list items, it could be helpful here
 
 
 // $("#hide").click(function(){
@@ -66,26 +86,3 @@ showPage();
 //     $(".student-list").show();
 // });
 
-
-// From the study guide:
-/* 
-// const showPage = (list, page) => {
-    // loop over items in the list parameter
-    // if the index of a list item is >= the index of the first item that should be shown on the page, && the list item index is <= the index of the last item that should be shown on the page, show it
-    // else hide it
-//}
-
-const appendPageLinks = (list) => {
-    // if pagination already exists, remove it
-    // determine how many pages are needed for the list by dividing the total number of list items by the max number of items per page
-    // create a div, give it the “pagination” class, and append it to the .page div
-    // add a ul to the “pagination” div
-    // for every page
-    // add li and a tags with the page number text
-    // add an event listener to each a tag, or add an event listener to the pagination div, and use event delegation to target the a tags to define what happens they are clicked
-    // calls the showPage function to display the appropriate page
-    // loop over pagination links to remove active class from all
-    // add the active class to the link that was just clicked, otherwise known as the
-    event.target
-}
- */
