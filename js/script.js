32d9400f80f40069ec642f9c1765bcae52c9a80f
Karl-Magnus Thorsnes <karl-magnus.thorsnes@kantar.com>
2018-11-05 15:32:44 +0100

    maybe someone could have a look

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ let numberOfStudents = document.getElementsByClassName('student-item').length;
 // Maximum number of students per page
 let studentsPerPage = 10;
 // Variable that calculates the numbers of pagination.  
-let totalPages = Math.ceil(numberOfStudents/studentsPerPage);
+let totalPages = Math.ceil(numberOfStudents / studentsPerPage);
 // Getting all student info
 let showStudent = document.getElementsByClassName('student-item');
 
@@ -20,7 +20,7 @@ let showPage = (list, pageNumber) => {
     max = (list * pageNumber);
     for (i = 0; i < numberOfStudents; i++)
         if (
-            (i >= min) && (i < max)
+            (i >= min) && (i <= max)
         ) {
             showStudent[i].style.display = "block";
         } else {
@@ -41,7 +41,7 @@ let appendPageLinks = (list) => {
     // creates new list and appends it to the newly created div
     let newUl = document.createElement("ul");
     newDiv.appendChild(newUl);
-    
+
     // loop for creating Li and a tags for pagination
     for (i = 0; i < list; i++) {
         // creates li and a tags for all pages
@@ -49,17 +49,17 @@ let appendPageLinks = (list) => {
         let newA = document.createElement("a");
         newUl.appendChild(newLi);
         newLi.appendChild(newA);
-        newA.innerHTML += i + 1; 
-        let links = document.querySelectorAll("a"); 
+        newA.innerHTML += i + 1;
+        let links = document.querySelectorAll("a");
         links[i].addEventListener("click", function () {
             for (i = 0; i < list; i++) {
-                let links = document.querySelectorAll("a"); 
+                let links = document.querySelectorAll("a");
                 links[i].classList.remove("active");
             }
             this.classList.add("active");
             currentPage = (document.getElementsByClassName('active')[0].innerHTML);
+            console.log("You are on page " + currentPage, "kuk " + studentsPerPage);
             showPage(studentsPerPage, currentPage);
-            console.log("You are on page " + currentPage);
         });
     };
 };
@@ -88,6 +88,9 @@ let searchField = () => {
     // Creates search functionality // Code base from:  https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_filter_list
 
     document.getElementById("btnid").addEventListener("click", function () {
+        // Removes error element
+        let removeErrorMessage = document.getElementsByClassName("error-message");
+        while (removeErrorMessage.length > 0) removeErrorMessage[0].remove();
         input = document.getElementById("Input");
         filter = input.value.toLocaleUpperCase();
         ul = document.getElementsByClassName("student-list")[0];
@@ -98,27 +101,25 @@ let searchField = () => {
             if (a.innerHTML.toUpperCase().indexOf(filter) >= 0) {
                 inputArray.push(input.value);
                 showStudent[i].style.display = "block";
-                
-             } else {
+
+            } else {
                 showStudent[i].style.display = "none";
                 console.log("no persons found");
-                
-                    }
+            }
         }
         numberOfStudents = inputArray.length;
-        if (numberOfStudents = 0) {
+        if (numberOfStudents == 0) {
             let noResultsDiv = document.createElement("div");
             noResultsDiv.className = "error-message"
             let noResultsDivPlacer = document.getElementsByClassName("student-list")[0];
-            let NoResultParagraph = document.createElement("p");
-            NoResultParagraph.id = "errorId"
-            noResultsDivPlacer.appendChild(NoResultParagraph);
-            document.getElementById("errorID").innerHTML = "YOUR TEXT";
-            //NoResultParagraph.innerHTML = "No student found";
-            };            
+            let noResultParagraph = document.createElement("p");
+            noResultParagraph.id = "errorId"
+            noResultsDivPlacer.appendChild(noResultsDiv);
+            noResultsDiv.appendChild(noResultParagraph);
+            noResultParagraph.innerHTML = "No student found";
+        };
         let totalPages = Math.ceil(numberOfStudents / studentsPerPage);
         console.log("the number of persons found:" + numberOfStudents, totalPages);
-        showPage(studentsPerPage);
         appendPageLinks(totalPages);
     });
 };
@@ -126,5 +127,5 @@ let searchField = () => {
 // Runs appenPageLinks for creating pagination
 appendPageLinks(totalPages);
 // Runs showPage function and starts on first page
-showPage(studentsPerPage, 1 );
+showPage(studentsPerPage, 1);
 searchField();
